7c58a44183bd933ee73d1bf334f0f8d96e59f4f2
YQ006 <hurui1@cloudwalk.cn>
2020-06-11 20:03:47 +0800

    增加新功能

--- mediaoption/mediaoption.cpp
+++ mediaoption/mediaoption.cpp
@@ -17,15 +17,10 @@ void mediaoption::initWindow(void)
 	timer = new QTimer(this);
 	void(QComboBox:: * pcomboBox)(int) = &QComboBox::currentIndexChanged;
 	QObject::connect(ui.comboBox, pcomboBox, this, &mediaoption::setScreen);
-	for (int j = 0; j < scr->getScrnum(); j++)
-	{
-		QObject::connect(scr->label[j], &PlayQlabel::leftclick, this, [=]() {
-			scrindex = j;
-			});
-	}
-
-	QObject::connect(ui.pushButton, &QPushButton::clicked, this, [=]() {
+	QObject::connect(ui.pushButton, &QPushButton::clicked, this, [=]() mutable {
+		
 		timer->start(15);
+		
 		if (decoder.Open(url))
 		{
 			QObject::connect(timer, SIGNAL(timeout()), SLOT(opencvdisplay()));
@@ -41,7 +36,7 @@ void mediaoption::initWindow(void)
 
 void mediaoption::opencvdisplay(void)
 {
-	qDebug() << scrindex;
+	
 	decoder.GetNextFrame(image);
 	QPixmap pixmap = QPixmap::fromImage(image);
 	QPixmap fitpixmap = pixmap.scaled(scr->label[scrindex]->size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
@@ -51,13 +46,20 @@ void mediaoption::opencvdisplay(void)
 
 void mediaoption::setScreen(int check)
 {
+	
 	timer->stop();
 	QString ind = ui.comboBox->currentText();
 	int index = ind.mid(0, ind.length() - 2).toInt();
 	scr->setScrnum(index);
 	scr->deletelayout();
 	scr->changeScreen(ui);
-	timer->start();
+	for (int j = 0; j < scr->getScrnum(); j++)
+	{
+		QObject::connect(scr->label[j], &PlayQlabel::leftclick, this, [=]() mutable {
+			scrindex = j;
+			});
+	}
+	timer->start(15);
 }
 mediaoption:: ~mediaoption()
 {
